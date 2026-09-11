8ed27e0247696be95883879d6aae66c6f27d456d
wls503pl <peile.wu.1990@gmail.com>
2026-09-11 15:55:36 +0800

    Update lc_239.

--- 33_搜索旋转排序数组/lc.cpp
+++ 33_搜索旋转排序数组/lc.cpp
@@ -3,25 +3,27 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left)/2;
             if (nums[mid] == target) return mid;
 
             if (nums[left] <= nums[mid]) {
-                // 左半部分 [left, mid] 有序
-                if (nums[left] <= target && target < nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                // 右半部分 [mid, right] 有序
-                if (nums[mid] < target && target <= nums[right]) {
+            }
+            else {
+                if (target > nums[mid] && target <= nums[right])
+                {
                     left = mid + 1;
-                } else {
+                }
+                else {
                     right = mid - 1;
                 }
             }
         }
+
         return -1;
     }
 };
